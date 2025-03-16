14ed67ec1f7c8a646f276a9fb388c7d3f9df5fc7
Tan-Furukawa <furukawatan@gmail.com>
2025-03-16 15:29:21 +0900

    update

--- python20250316.py
+++ python20250316.py
@@ -1,6 +1,36 @@
 # %%
 import numpy as np
 
-1 + 1
+# 型-----------------------------
 
-# %%
+# 整数型
+x1: int = 1
+# 小数型
+x2: float = 1.0
+# bool型
+x3: bool = False
+# 文字型
+x4: str = "hello goodbye"
+
+
+
+# 二項演算子----------------------------
+x = 2 * 5
+y = 3
+
+## (A, A) -> A型
+z = x * y
+print(z)
+z = x - y
+print(z)
+z = x + y
+print(z)
+
+
+## (A, A) -> float型
+z = x / y
+print(z)
+
+## (A, A) -> bool型
+z = x == y
+print(z)
