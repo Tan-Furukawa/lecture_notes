e868dcabbc63fa9030c4871f7dc0c239c2eb93dc
Tan-Furukawa <furukawatan@gmail.com>
2025-03-16 17:30:06 +0900

    update

--- python20250316.py
+++ python20250316.py
@@ -1,6 +1,9 @@
 # %%
 import numpy as np
+import matplotlib.pyplot as plt
 from numpy.typing import NDArray
+# %%
+
 
 # 型-----------------------------
 # :型の形で書く。書いても書かなくてもいい
@@ -15,9 +18,16 @@ x3: bool = False
 x4: str = "hello goodbye"
 # None型 (応用、気にしなくていい)
 x5: None = None
-# NDArray型
-x6: NDArray = np.linspace(0, 10, 100)
-#%%
+# NDArray型 (numpy array専用)
+# np.linspace(start: float, end: float, 数: int)
+x6: NDArray = np.linspace(0.0, 10.0, 100)
+x7: NDArray = np.random.random(100)
+x8: NDArray = np.zeros((100,))
+x9: NDArray = np.ones((100,))
+
+# これ以外にも無限にある
+
+# %%
 
 # 二項演算子----------------------------
 # 整数や実数に対して
@@ -37,11 +47,12 @@ z = x == y
 print(z)
 
 ## (bool, bool) -> bool型
-# and 演算子
+# and 演算子（かつ)
 print((x == y) and False)
+# print( x*y and False) 意味不明だから使わない
 
 ## (bool, bool) -> bool型
-# or 演算子
+# or 演算子 （または）
 print((x == y) or False)
 
 # %%
@@ -50,6 +61,7 @@ p: str = "hello"
 q: str = "goodbye"
 print(p + q)
 print(p == q)
+# print(p * 3) なるべくやらない
 # print(p - q)  # error
 # print(p / q)  # error
 
@@ -59,11 +71,37 @@ print(p == q)
 
 s = False
 print(not s)
+# print(not 1)
 
 # 組み合わせ
 
 # %%
 # トリッキーな演算子
+# []演算子
 # (Array of A, int) -> A
-vec = np.linspace(0, 1.0, 100)
+vec = np.linspace(0.0, 1.0, 100)
 print(vec[10])
+
+
+# %%
+
+# 関数
+
+
+def add(x: int, y: int) -> int:
+    return x + y
+
+
+# とりあえずは型は書かなくてもよい
+def add2(x, y):
+    return x + y
+
+def add_double(x: int, y: int) -> int:
+    return x + y * 2
+
+
+print(add(add(1, 2), 3))
+
+print(add_double(10, 11))
+
+# %%
